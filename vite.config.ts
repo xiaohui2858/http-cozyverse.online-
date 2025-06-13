bc16cf5813fe7b2766cc61a8652c90d663f563bf
xiaohui2858 <1341078387@qq.com>
2025-06-13 23:05:32 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'build' ? '/http-cozyverse.online-/' : '/',
+base: '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },

9f06e634e99659cf94ced96cfc1ea7cd77121061
xiaohui2858 <1341078387@qq.com>
2025-06-13 15:48:46 +0800

    11

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
   base: '/',
   optimizeDeps: {
@@ -12,4 +12,4 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets'
   }
-});
+}));
